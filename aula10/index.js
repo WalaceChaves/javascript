1bc751dd0ab6606fdf80c97ae207728f5930963b
Walace Chaves <walace.chaves@outlook.com.br>
2023-05-09 09:26:11 -0300

    Alter

---
+++ aula10/index.js
@@ -0,0 +1,7 @@
+let data_atual = new Date();
+
+let dia = data_atual.getDay();
+let mes = data_atual.getMonth();
+let ano = data_atual.getFullYear();
+
+console.log(`Hojé é dia ${dia}/${mes}/${ano}`)
